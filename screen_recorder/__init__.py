fd0c138fd9d4bbcde20a130ae8e82bb8f28d5d68
ParticleG <particle_g@outlook.com>
2023-06-02 16:00:45 +0800

    - Use dxcam to improve capture performance

--- screen_recorder/__init__.py
+++ screen_recorder/__init__.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 from threading import Thread, Event
 
 import cv2
+import dxcam
 import numpy as np
-import pyautogui
+from pil.Image import Image
 
 
 class ImageHandler(ABC):
@@ -13,11 +14,13 @@ class ImageHandler(ABC):
 
 
 class ScreenRecorder:
+    __camera: dxcam.DXCamera
     __handler_map: dict[str, callable] = {}
     __stop_event: Event = Event()
     __thread_handle: Thread
 
     def __init__(self):
+        self.__camera = dxcam.create()
         self.__thread_handle = Thread(target=self.__capture_screen)
 
     def register(self, name, handler: ImageHandler):
@@ -42,9 +45,11 @@ class ScreenRecorder:
 
     def __capture_screen(self):
         while True:
-            image = cv2.cvtColor(np.asarray(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
-            for handler in self.__handler_map.values():
-                handler(image)
-            if cv2.waitKey(10) & 0xFF == ord("q") or self.__stop_event.is_set():
+            image = self.__camera.grab()
+            if image is not None:
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                for handler in self.__handler_map.values():
+                    handler(image)
+            if cv2.waitKey(1) & 0xFF == ord("q") or self.__stop_event.is_set():
                 self.__stop_event.clear()
                 return
